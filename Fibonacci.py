597ab02020e3afc5238b0b74432d1b260eafda74
Harthi7 <abdullah-harthi@live.com>
2020-05-04 19:58:22 -0700

    deleting files

--- Fibonacci.py
+++
@@ -1,9 +0,0 @@
-F = int(input("Enter how many numbers to output of the fibonacci series:\n"))
-One = 0
-Two = 1
-
-for i in range(F):
-    print(One)
-    store = One
-    One = Two
-    Two = store+Two
